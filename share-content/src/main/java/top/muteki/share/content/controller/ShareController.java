fac8a652baa13be913688d21be16ee17f12a7c11
fsfs <zixin02700@gmail.com>
2023-10-08 21:13:33 +0800

    编写获取首页分享数据接口（下）

--- share-content/src/main/java/top/muteki/share/content/controller/ShareController.java
+++ share-content/src/main/java/top/muteki/share/content/controller/ShareController.java
@@ -1,36 +1,73 @@
 package top.muteki.share.content.controller;
 
+import cn.hutool.json.JSONObject;
 import jakarta.annotation.Resource;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import top.muteki.share.user.resp.CommonResp;
+import top.muteki.share.user.util.JwtUtil;
 import top.muteki.share.content.domain.entity.Notice;
 import top.muteki.share.content.domain.entity.Share;
 import top.muteki.share.content.service.NoticeService;
 import top.muteki.share.content.service.ShareService;
-import top.muteki.share.user.resp.CommonResp;
 
 import java.util.List;
 
+/**
+ * @author mqxu
+ * @date 2023/10/8
+ * @description ShareService
+ **/
 @RestController
-@RequestMapping("/share")
+@RequestMapping(value = "/share")
+@Slf4j
 public class ShareController {
     @Resource
     private NoticeService noticeService;
+
     @Resource
     private ShareService shareService;
+
+    private final int MAX = 100;
+
     @GetMapping(value = "/notice")
-    public CommonResp<Notice> getLatestNotice(){
+    public CommonResp<Notice> getLatestNotice() {
         CommonResp<Notice> commonResp = new CommonResp<>();
         commonResp.setData(noticeService.getLatest());
         return commonResp;
     }
+
     @GetMapping("/list")
-    public CommonResp<List<Share>> getShareList(@RequestParam(required = false)String title){
-        CommonResp<List<Share>> commonResp=new CommonResp<>();
-        Long userId=2L;
-        commonResp.setData(shareService.getList(title,userId));
+    public CommonResp<List<Share>> getShareList(@RequestParam(required = false) String title,
+                                                @RequestParam(required = false, defaultValue = "1") Integer pageNo,
+                                                @RequestParam(required = false, defaultValue = "3") Integer pageSize,
+                                                @RequestHeader(value = "token", required = false) String token) {
+        if (pageSize > MAX) {
+            pageSize = MAX;
+        }
+        int userId = getUserIdFromToken(token);
+        CommonResp<List<Share>> commonResp = new CommonResp<>();
+        commonResp.setData(shareService.getList(title, pageNo, pageSize, (long) userId));
         return commonResp;
+    }
+
+    /**
+     * 封装一个从 token 中提取 userId 的方法
+     *
+     * @param token token
+     * @return userId
+     */
+    private int getUserIdFromToken(String token) {
+        log.info(">>>>>>>>>>> token" + token);
+        int userId = 0;
+        String noToken = "no-token";
+        if (!noToken.equals(token)) {
+            JSONObject jsonObject = JwtUtil.getJSONObject(token);
+            log.info("解析到 token 的 json 数据为：{}", jsonObject);
+            userId = (Integer) jsonObject.get("id");
+        } else {
+            log.info("没有 token");
+        }
+        return userId;
     }
 }
--- share-content/src/main/java/top/muteki/share/content/service/ShareService.java
+++ share-content/src/main/java/top/muteki/share/content/service/ShareService.java
@@ -2,7 +2,7 @@ package top.muteki.share.content.service;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.github.pagehelper.util.StringUtil;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 import top.muteki.share.content.domain.entity.MidUserShare;
@@ -13,36 +13,62 @@ import top.muteki.share.content.mapper.ShareMapper;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * @author muteki
+ * @date 2023/10/8
+ * @description ShareService
+ **/
 @Service
 public class ShareService {
     @Resource
     private ShareMapper shareMapper;
+
     @Resource
     private MidUserShareMapper midUserShareMapper;
 
-    public List<Share> getList(String title, Long userId) {
+    /**
+     * 查询某个用户首页可见的资源列表
+     *
+     * @param title  标题
+     * @param userId 用户id
+     */
+    public List<Share> getList(String title, Integer pageNo, Integer pageSize, Long userId) {
+        // 构造查询条件
         LambdaQueryWrapper<Share> wrapper = new LambdaQueryWrapper<>();
+        // 按照 id 降序查询所有数据
         wrapper.orderByDesc(Share::getId);
-        if (StringUtil.isNotEmpty(title)) {
+        // 如标题关键字不空，则加上模糊查询条件，否则结果即所有数据
+        if (title != null) {
             wrapper.like(Share::getTitle, title);
         }
+        // 过滤出所有已经通过审核的数据并需要显示的数据
         wrapper.eq(Share::getAuditStatus, "PASS").eq(Share::getShowFlag, true);
-        List<Share> shares = shareMapper.selectList(wrapper);
+
+        // 内置的分页对象
+        Page<Share> page = Page.of(pageNo, pageSize);
+        // 执行按条件查询
+        List<Share> shares = shareMapper.selectList(page,wrapper);
+
+        // 处理后的 Share 数据列表
         List<Share> sharesDeal;
+        // 1. 如果用户未登录，那么 downloadUrl 全部设为 null
         if (userId == null) {
             sharesDeal = shares.stream().peek(share -> share.setDownloadUrl(null)).collect(Collectors.toList());
-        } else {
+        }
+        // 2. 如果用户登录了，那么查询 mid_user_share，如果没有数据，那么这条 share 的 downloadUrl 也设为 null
+        // 只有自己分享的资源才能直接看到下载链接，否则显示"兑换"
+        else {
             sharesDeal = shares.stream().peek(share -> {
                 MidUserShare midUserShare = midUserShareMapper.selectOne(new QueryWrapper<MidUserShare>().lambda()
                         .eq(MidUserShare::getUserId, userId)
                         .eq(MidUserShare::getShareId, share.getId()));
+
                 if (midUserShare == null) {
                     share.setDownloadUrl(null);
                 }
-
-
             }).collect(Collectors.toList());
         }
+
         return sharesDeal;
     }
 }
